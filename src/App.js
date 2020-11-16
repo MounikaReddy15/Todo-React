0674b787fd40c1e9c4d233b1b0f9e8d950561de7
unknown <mounikareddygonae15@gmail.com>
2020-11-16 19:10:25 +0530

    add delete functionality

--- src/App.js
+++ src/App.js
@@ -14,26 +14,41 @@ class App extends Component {
   handleOnChange=(e)=>{
      
     this.setState({task:[e.target.value]});
+    
   }
 
   handleClick=()=>{
     const {task,tasks}=this.state;
     // check if task is empty
 
-    if(task===''){alert("task is empty")}
-
-    else{
-     tasks.push(task);
-    // if not add this task in tasks array
-    this.setState({tasks:tasks});
+    if(task === ''){
+      alert("task is empty")
     }
 
+  else{
+    
+    // if not add this task in tasks array
+    tasks.push(task);
+    this.setState({tasks:tasks});
+    
+  }
+    
   }
 
   handleDelete=(task)=>{
+    const {tasks}=this.state;
     // use filter to filter out task from array
+    const deleteTask = tasks.filter(todo => todo !== task)
+    this.setState({tasks:deleteTask});
 
   }
+
+  handleEdit=(todo)=>{
+    
+    // const {task,tasks}=this.state;
+    // todo = 
+  }
+
   render() {
    const {task,tasks}=this.state;
     console.log('task',task);
@@ -43,8 +58,11 @@ class App extends Component {
         
         <input  onChange={this.handleOnChange}/>
         <button onClick={this.handleClick}>Add Task</button>
- {tasks.map((task)=>
-       <Task task={task} handleDelete={this.handleDelete}/>
+       {tasks.map((task)=>
+       <Task task={task} 
+       handleEdit={this.handleEdit}
+       handleDelete={this.handleDelete}
+       />
  )}
       </div>
     );
--- src/components/task.js
+++ src/components/task.js
@@ -2,11 +2,13 @@
 import * as React from 'react';
 
 export const Task = (props) => {
-    const {task,handleDelete}=props;
+    const {task,handleDelete, handleEdit}=props;
     return (
-        <div>
-        
-    <p style={{display:"inline-block"}}>{task}</p> <button style={{display:"inline-block"}} onClick={()=>handleDelete(task)}>Delete</button>
-        </div>
+    <div>
+    <p style={{display:"inline-block"}}>{task}</p> 
+    <button style={{display:"inline-block"}} onClick={()=>handleEdit(task)}>Edit</button>
+    <button style={{display:"inline-block"}} onClick={()=>handleDelete(task)}>Delete</button>
+    
+    </div>
     );
 };
